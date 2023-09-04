f7bdcd42efe15277be1517ebcc9f22b974b15ae4
Jordan Mck <ghostdagunna@gmail.com>
2023-09-04 12:20:49 -0500

    Initial Commit

---
+++ ScrumDingerApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ScrumDingerApp
+//
+//  Created by Jordan Mck on 9/4/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ScrumDingerApp/ScrumDingerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumDingerAppApp.swift
+//  ScrumDingerApp
+//
+//  Created by Jordan Mck on 9/4/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumDingerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
